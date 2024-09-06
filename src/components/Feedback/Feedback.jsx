19aafa426569b338726d85f4a32553292c377617
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2024-09-07 01:55:49 +0200

    add module.css

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,6 +1,7 @@
+import s from "./Feedback.module.css";
 const Feedback = ({ feedback, positiveFeedbackPercentage }) => {
   return (
-    <div>
+    <div className={s.wrap}>
       <ul>
         <li>Good: {feedback.good} </li>
         <li>Netural: {feedback.neutral} </li>
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,10 +1,16 @@
+import s from "./Options.module.css";
 const Options = ({ updateFeedback, totalFeedback, resetFeedback }) => {
   return (
     <>
-      <button onClick={() => updateFeedback("good")}>Good</button>
-      <button onClick={() => updateFeedback("neutral")}>Neutral</button>
-      <button onClick={() => updateFeedback("bad")}>Bad</button>
-      {totalFeedback && <button onClick={resetFeedback}>Reset</button>}
+      <ul className={s.button}>
+        <button onClick={() => updateFeedback("good")}>Good</button>
+
+        <button onClick={() => updateFeedback("neutral")}>Neutral</button>
+
+        <button onClick={() => updateFeedback("bad")}>Bad</button>
+
+        {totalFeedback && <button onClick={resetFeedback}>Reset</button>}
+      </ul>
     </>
   );
 };
